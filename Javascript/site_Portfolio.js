f61389e713ca06df614b4568f5573d0899daed7e
clelie <cleliechassignet@hotmail.fr>
2020-05-13 15:19:06 +0200

    Importation site sans vidéos

---
+++ Javascript/site_Portfolio.js
@@ -0,0 +1,24 @@
+const rect1 = document.querySelector('.rect1');
+const rect1b = document.querySelector('.rect1b');
+
+const rect2 = document.querySelector('.rect2');
+const rect2b = document.querySelector('.rect2b');
+
+const video = document.querySelector('.video-container');
+
+rect1.onmouseover = function (){
+	rect1b.style.display='flex';
+}
+ 
+rect1.onmouseout = function (){
+	rect1b.style.display='none';
+}
+
+rect2.onmouseover = function (){
+	rect2b.style.display='flex';
+}
+ 
+rect2.onmouseout = function (){
+	rect2b.style.display='none';
+}
+
